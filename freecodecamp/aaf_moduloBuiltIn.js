7fe09f5a35b0ec092856705e5d1d375c0fe74a7f
ulisesGutierrez <techwithulises@gmail.com>
2023-07-17 18:20:31 -0600

    setImmediate

--- freecodecamp/aaf_moduloBuiltIn.js
+++ freecodecamp/aaf_moduloBuiltIn.js
@@ -49,20 +49,39 @@ Higher-order programming:
 Es una técnica de programación que consiste en pasar una función como argumento a otra función.
 
 setTimeout(): Para ejecutar codigo luego de un numero especifico de milisegundos.
+
 Estructura:
 setTimeout(fuction, delay, [arg], [...]))
 o
 setTimeout(callback, delay, [arg], [...])
 */
-function timeDelay(topic) {
+function usingSettimeout(topic) {
     console.log(`I'm learning ${topic}`);
 }
 
-//setTimeout(timeDelay, 1000, "Node.js");//Ejecuta la función timeDelay luego de 1 segundo.
-setTimeout(timeDelay, 5000, "Node.js");//Ejecuta la función timeDelay luego de 5 segundos.
+//setTimeout(usingSettimeout, 1000, "Node.js");//Ejecuta la función usingSettimeout luego de 1 segundo.
+setTimeout(usingSettimeout, 5000, "Node.js");//Ejecuta la función usingSettimeout luego de 5 segundos.
 
 function addNumber(a, b) {
     console.log(a + b);
 }
 
 setTimeout(addNumber, 15000, 5, 10);//Ejecuta la función addNumber luego de 15 segundos.
+
+/*
+setImmediate(): Para ejecutar codigo asincrono en la proxima iteración del ciclo de eventos(lo mas pronto posible).
+Se ejecuta despues del codigo sincrono.
+Se ejecuta lo mas rapido posible de forma asincrona.
+Estructura:
+setImmediate(fuction, [arg], [...]))
+o
+setImmediate(callback, [arg], [...])
+*/
+
+function usingsetimmediate(topic) {
+    console.log(`I'm learning ${topic}`);
+}
+
+console.log("Before setImmediate");
+setImmediate(usingsetimmediate, "Node.js");//Ejecuta la función usingsetimmediate en la proxima iteración del ciclo de eventos.
+console.log("After setImmediate");//Se ejecuta antes que la función usingsetimmediate.
