8d087840ae75baa341ab4cf04b29cefd46f667ff
ulisesGutierrez <techwithulises@gmail.com>
2023-07-17 22:50:53 -0600

    npm

---
+++ freecodecamp/aal_npm.js
@@ -0,0 +1,9 @@
+/*
+Npm:
+Es el archivo de software mas grande del mundo que contiene paquetes que
+puedes instalar y usar con node.js.
+Tambien es una herramienta para la linea de comandos.
+
+Pawquete:
+Archivo o directorio descrito por un archivo package.json.
+*/
