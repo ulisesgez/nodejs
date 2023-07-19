0201ae8b471f10f22183c694eda2a6272f800549
ulisesGutierrez <techwithulises@gmail.com>
2023-07-18 20:46:52 -0600

    clear

---
+++ aan_json.js
@@ -0,0 +1,10 @@
+/*
+JSON (Javascript Object Notation):
+Formato de texto usado para almacenar y transportar información.
+Json esta badado en la notacion de objetos de Javascript, pero como formato de texto.
+Nos permite almacenar objetos de javascript como texto.
+Es independiente del lenguaje de programacion con el cual estas trabajando, es decir, puede ser usado por la mayoria de los lenguajes de programación.
+*/
+const course = require('./aam_json.json');
+
+console.log(course);
