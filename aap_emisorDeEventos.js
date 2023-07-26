6d11cf7fc8fa18987cf8f9a552f8d50f4bb0621a
ulisesGutierrez <techwithulises@gmail.com>
2023-07-25 21:56:21 -0600

    eventos

--- aap_emisorDeEventos.js
+++ aap_emisorDeEventos.js
@@ -1,3 +1,12 @@
 const EventEmitter = require('events');
 //console.log('EventEmitter: ', EventEmitter);// EventEmitter:  [Function: EventEmitter]
+//Modulo events y princio basico de los emisores de eventos:
 const EmisorDeEventos = new EventEmitter();
+
+EmisorDeEventos.on('compra', () => {
+    console.log('Se realizo una compra', );//Funcion que maneja el evento o event handler
+});
+
+EmisorDeEventos.emit('compra');// Se realizo una compra
+//EmisorDeEventos.emit('compra'); // Como esta comentado no se ejecuta
+EmisorDeEventos.emit('compra');// Se realizo una compra
