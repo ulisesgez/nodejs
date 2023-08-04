5078406207961ba3970938ebf02d20ffbfaddcd4
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-08-03 22:55:20 -0600

    promesas

--- aar_asyncAwait.js
+++ aar_asyncAwait.js
@@ -1,2 +1,46 @@
 /*
+Async Await:
+
+Usando Promesas:
 */
+
+function comprarProducto(producto) {
+    return new Promise((resolve, reject) => {
+        let numeroOrden = Math.floor(Math.random() * 1000000) + 1;
+        console.log('Iniciando compra');
+        console.log(`Odenando ${producto}...`);
+        setTimeout(() => {
+            if (producto === 'Jugo') {
+                console.log(`El ${producto} ha sido ordenado`);
+                resolve(`Numero de orden: ${numeroOrden}`);
+            } else {
+                reject(`Lo sentimos, por el momento se encuentra agotado el producto solicitado (${producto})`);
+            }
+        }, 3000);
+    });
+}
+
+function pagarProducto(respuesta) {
+    return new Promise((resolve, reject) => {
+        respuesta = respuesta.toLowerCase();
+        console.log(`Pagando Producto...`);
+        setTimeout(() => {
+            resolve(`El producto con ${respuesta} ha sido pagado exitosamente`);
+        }, 4000);
+    });
+}
+
+//Chainig Promises:
+comprarProducto('Huevo')
+    .then(res => {
+        console.log(res);//Numero de orden: 123456
+        return pagarProducto(res);//Pagando Producto...
+    })
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+//Usando Async Await:
