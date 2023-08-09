4f0cb375105c7c20ae8246d5e01ddf08ce43bd6b
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-08-08 20:56:16 -0600

    introduccion

---
+++ aaa_introduccion.js.js
@@ -0,0 +1,21 @@
+/*
+Node.js
+Ideado como un entorno de ejecución de JavaScript orientado a eventos asíncronos,
+Node.js está diseñado para crear aplicaciones escalables en red.
+
+REPL:
+- Read
+- Eval
+- Print
+- Loop
+Herramienta interactiva, que nos permite ejecutar y probar el códogo del lenguaje en el
+que nos encontremos.
+Podemos acceder al repl de node.js desde la consola de comandos,
+escribiendo node y pulsando enter.
+
+A continuación haremos nuestro primer hola mundo en node.js, no confundirse con el
+concepto anterior ya que no entraremos al repl, ejecutaremos el archivo desde la consola:
+node hola_mundo.js que en este caso seria node aaa_introduccion.js
+*/
+let nombre = "Ulises";
+console.log("Hola " + nombre);
