8f75e6150b3bde8ce0aa863c9a2659178f7036af
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-08-09 10:50:22 -0600

    modulos

--- aah_modulos.js
+++ aah_modulos.js
@@ -1,4 +1,29 @@
-//Exportar elemento:
+/*
+Modulos:
+- Son archivos que contienen codigo de JS.
+- Pueden ser requeridos desde otros archivos.
+- Pueden ser exportados desde un archivo.
+
+CommonJS:
+- Es una especificacion que define como deben ser los modulos de JS.
+- Es la especificacion que NodeJS implementa para manejar modulos.
+- Cada archivo es un modulo.
+- Cada modulo tiene su propio scope.
+- Para exportar un elemento se debe agregar al objeto module.exports.
+- Para importar un modulo se debe usar la funcion require.
+- Esta es la vieja forma de manejar modulos en JS.
+
+ESModules:
+- Es una especificacion que define como deben ser los modulos de JS.
+- Es la especificacion que los navegadores implementan para manejar modulos.
+- Cada archivo es un modulo.
+- Cada modulo tiene su propio scope.
+- Para exportar un elemento se debe agregar al objeto export.
+- Para importar un modulo se debe usar la funcion import.
+- Esta es la nueva forma de manejar modulos en JS.
+
+Exportar elemento:
+*/
 function saludar(nombre = "Usuario") {
     return `Hola, ${nombre}!`;
 }
