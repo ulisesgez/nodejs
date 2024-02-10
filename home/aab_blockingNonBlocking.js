9d2d4e10aee828c631d687bfd3ab125a33ee2872
ulisesGez <gu362937@uaeh.edu.mx>
2024-02-10 16:20:39 -0600

    node

--- home/aab_blockingNonBlocking.js
+++ home/aab_blockingNonBlocking.js
@@ -2,12 +2,12 @@
 Blocking vs Non-Blocking:
 Relacion que existe en como node.js va a ejecutar las operaciones de entrada y salida.
 
-Asincrono o Bloqueante:
+Sincrono o Bloqueante:
 - Se ejecuta una operacion de entrada y salida.
 - Se espera a que se termine de ejecutar la operacion de entrada y salida.
 - Se continua con la ejecucion del programa.
 
-Sincrono o No Bloqueante:
+Asincrono o No Bloqueante:
 - Se ejecuta una operacion de entrada y salida.
 - Se continua con la ejecucion del programa.
 - Cuando la operacion de entrada y salida termina, se ejecuta una funcion de callback.
